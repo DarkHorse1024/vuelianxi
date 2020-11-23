1c7f6e0aee74a5856423a9eadad5f3ec45d3e3ec
朱建新 <zhujianxin@zhujianxindeMacBook-Pro.local>
2020-11-23 23:28:16 +0800

    vueccc第一次提交

---
+++ src/cc.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <ul>
+      <li>我是cc页面</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  methods: {},
+  computed: {},
+  filters:{},
+  watch: {},
+  created(){},
+  mounted(){},
+  updated(){}
+}
+</script>
+
+<style scoped>
+
+</style>>
+
