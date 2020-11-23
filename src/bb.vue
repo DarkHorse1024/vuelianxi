0ecf57ac7a0bce2f4b67cb616f87ab47cfc1b9b2
朱建新 <zhujianxin@zhujianxindeMacBook-Pro.local>
2020-11-23 23:17:44 +0800

    分支提交

---
+++ src/bb.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="">
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  methods: {},
+  computed: {},
+  filters:{},
+  watch: {},
+  created(){},
+  mounted(){},
+  updated(){}
+}
+</script>
+
+<style scoped>
+
+</style>>
+
